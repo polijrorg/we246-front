c9862ab43a0c03b80f305ed3250a5d66dd239cf7
matheusguilherme <matheus.nascimento@polijunior.com.br>
2024-07-25 12:20:06 -0300

    endereco

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -42,6 +42,34 @@ interface ICreateResponse {
   updated_at: string;
 }
 
+interface User {
+  id: string;
+  name: string;
+  cpf_cnpj: string; // Assuming this is a Brazilian identification number
+  phone: string;
+  password: string; // Please note that storing passwords in plain text is not secure
+  addressId: string;
+  account_type: string;
+  created_at: string; // Assuming this is a date and time in ISO 8601 format
+  updated_at: string; // Assuming this is a date and time in ISO 8601 format
+  address: Address; // This is a nested interface
+}
+
+interface Address {
+  id: string;
+  street: string;
+  number: string;
+  complement: string; // Optional property for apartment number etc.
+  neighborhood: string;
+  city: string;
+  state: string;
+  zip_code: string;
+  created_at: string; // Assuming this is a date and time in ISO 8601 format
+  updated_at: string; // Assuming this is a date and time in ISO 8601 format
+}
+
+
+
 export default class UserService {
   static async login(data: ILoginRequest): Promise<IUserResponse | string> {
     try {
@@ -65,6 +93,22 @@ export default class UserService {
       const response: AxiosResponse<ICreateResponse> = await api.post(
         '/users/register',
         data
+      );
+      if (response.status >= 200 && response.status < 300) {
+        return response.data;
+      } else {
+        throw new Error('There was a problem with the registration');
+      }
+    } catch (error) {
+      console.error('Erro ao registrar o usuário', error);
+      return 'Erro ao registrar o usuário';
+    }
+  }
+
+  static async getById(id: string): Promise<User | string> {
+    try {
+      const response: AxiosResponse<User> = await api.post(
+        `/users/${id}`,
       );
       if (response.status >= 200 && response.status < 300) {
         return response.data;
